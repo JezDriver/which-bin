3f443ad85e7b6e42b6f1288ca6a6cb327fc82507
Jeremy Driver <jeremypdriver@gmail.com>
2023-11-17 09:07:41 +1030

    nicer error handling

--- which-bin.php
+++ which-bin.php
@@ -5,21 +5,30 @@ $dates = json_decode(file_get_contents('dates-2023.json'), true);
 date_default_timezone_set('Australia/Adelaide');
 $today = date('d/m/Y');
 
-$todaysBin = "";
+$todaysBin = "Unknown";
+$dateFound = false;
 foreach ($dates as $date) {
   if ($date['date'] === $today) {
     $todaysBin = $date['bin'];
+    $dateFound = true;
   }
 }
 
-if(!empty($todaysBin) && ($todaysBin === "Recycling" || $todaysBin === "Green")) {
-  $emoji = "";
-  if($todaysBin === "Recycling") {
-    $emoji = "🟡";
+if($dateFound) {
+  if ($todaysBin === "Recycling" || $todaysBin === "Green") {
+    $emoji = "";
+    if($todaysBin === "Recycling") {
+      $emoji = "🟡";
+    }
+    elseif($todaysBin === "Green") {
+      $emoji = "🟢";
+    }
   }
-  elseif($todaysBin === "Green") {
-    $emoji = "🟢";
+  else {
+    $emoji = "❓";
   }
-  
-  echo file_get_contents('https://trigger.macrodroid.com/bb1b07a4-fbec-4696-aac9-5f7bcefe9fb5/whichbin?whichBin=' . $todaysBin . '&binEmoji=' . urlencode($emoji));
 }
+else {
+  $emoji = "❓";
+}
+echo file_get_contents('https://trigger.macrodroid.com/bb1b07a4-fbec-4696-aac9-5f7bcefe9fb5/whichbin?whichBin=' . $todaysBin . '&binEmoji=' . urlencode($emoji));
