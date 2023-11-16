8ee8d0f6657ca5142a6174f76dd087f484b69167
Jeremy Driver <jeremypdriver@gmail.com>
2023-11-17 09:41:38 +1030

    Add comments

--- which-bin.php
+++ which-bin.php
@@ -1,11 +1,15 @@
 <?php 
-
+// Get Device ID from file (secrets)
 $macrodroidDeviceId = file_get_contents('macrodroid/macrodroid-device-id.txt');
+
+// Get dates data
 $dates = json_decode(file_get_contents('data/dates-2023.json'), true);
 
+// Get today's date
 date_default_timezone_set('Australia/Adelaide');
 $today = date('d/m/Y');
 
+// Loop over dates, checking for today's bin
 $todaysBin = "Unknown";
 $dateFound = false;
 foreach ($dates as $date) {
@@ -16,7 +20,9 @@ foreach ($dates as $date) {
 }
 
 if($dateFound) {
+  // If date was found, set the emoji icon
   if ($todaysBin === "Recycling" || $todaysBin === "Green") {
+    // Colour code different bins
     $emoji = "";
     if($todaysBin === "Recycling") {
       $emoji = "🟡";
@@ -26,10 +32,17 @@ if($dateFound) {
     }
   }
   else {
+    // Fallback if bin isn't recycling/green (the data is mungy oops)
     $emoji = "❓";
   }
 }
 else {
+  // Fallback for if the date wasn't found in the data
   $emoji = "❓";
 }
+
+// Trigger the webhook
+// - passes device ID
+// - sets whichBin macrodroid var to today's bin
+// - sets binEmoji macrodroid var to corresponding emoji
 echo file_get_contents('https://trigger.macrodroid.com/' . $macrodroidDeviceId . '/whichbin?whichBin=' . $todaysBin . '&binEmoji=' . urlencode($emoji));
